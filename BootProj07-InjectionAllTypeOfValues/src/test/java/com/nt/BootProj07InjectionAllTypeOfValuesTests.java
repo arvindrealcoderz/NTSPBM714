47380dd2a01d9b0fed7506d887feabcdda7be186
arvindtl <Arvind@DESKTOP-4NONCLR>
2021-06-18 09:34:30 +0530

    injection All tyoe of values by using application properties by arvind verma

--- BootProj07-InjectionAllTypeOfValues/src/test/java/com/nt/BootProj07InjectionAllTypeOfValuesTests.java
+++ BootProj07-InjectionAllTypeOfValues/src/test/java/com/nt/BootProj07InjectionAllTypeOfValuesTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootProj07InjectionAllTypeOfValuesApplicationTests {
+class BootProj07InjectionAllTypeOfValuesTests {
 
 	@Test
 	void contextLoads() {
--- BootProj07-InjectionAllTypeOfValuesByUsingYml/src/main/java/com/nt/InjectionAllTypeOfValuesApplication.java
+++ BootProj07-InjectionAllTypeOfValuesByUsingYml/src/main/java/com/nt/InjectionAllTypeOfValuesApplication.java
@@ -7,11 +7,11 @@ import org.springframework.context.ApplicationContext;
 import com.nt.beans.Employee;
 
 @SpringBootApplication
-public class BootProj07InjectionAllTypeOfValuesApplication {
+public class InjectionAllTypeOfValuesApplication {
 
 	public static void main(String[] args) {
 		//get IOC container
-		ApplicationContext ctx= SpringApplication.run(BootProj07InjectionAllTypeOfValuesApplication.class, args);
+		ApplicationContext ctx= SpringApplication.run(InjectionAllTypeOfValuesApplication.class, args);
 		Employee emp=ctx.getBean("emp",Employee.class);
 		System.out.println(emp);
 	
